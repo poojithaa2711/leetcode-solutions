5b712a07c59623f6d6b9c5e0b0149fef8bf3b3af
PENDYALA PUJITHA DEVI <23mh1a4946@acoe.edu.in>
2026-07-24 18:03:48 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 700143-bubble-sort-GFG/700143-bubble-sort.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        // code here
+        for(int i = 1; i < arr.length; i++){
+            for(int j = 0; j < arr.length - i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
