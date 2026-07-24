59699ffd285cbcda35970d4fb2087159d72dcf77
PENDYALA PUJITHA DEVI <23mh1a4946@acoe.edu.in>
2026-07-24 17:08:28 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 705381-array-traversal-reverse-GFG/705381-array-traversal-reverse.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void arrayTraversalReverse(int[] arr) {
+        // Code here
+        for(int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
