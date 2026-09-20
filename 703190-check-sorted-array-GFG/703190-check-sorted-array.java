fc063216f8e8e669a5e912064ff90d9a3ff2b0bc
PENDYALA PUJITHA DEVI <23mh1a4946@acoe.edu.in>
2026-09-20 18:03:10 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 703190-check-sorted-array-GFG/703190-check-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        // code here
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
