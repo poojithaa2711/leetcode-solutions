13172d506ad0edee34a1ae40cb0bde7411dcc05e
PENDYALA PUJITHA DEVI <23mh1a4946@acoe.edu.in>
2026-09-20 17:57:31 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703089-second-largest-GFG/703089-second-largest.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        if(largest<num){
+            
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
